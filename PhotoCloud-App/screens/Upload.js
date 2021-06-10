663e20561d365082904173fc0d236b869824aff4
Ricardo Leite <ricardo.leitejr95@gmail.com>
2021-06-09 20:49:46 -0600

    Add upload screen and server for content

--- PhotoCloud-App/screens/Upload.js
+++ PhotoCloud-App/screens/Upload.js
@@ -1,41 +1,101 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Button
+} from "react-native";
+import * as ImagePicker from "react-native-image-picker";
 
+const Upload = ({ navigation }) => {
+  const [photo, setPhoto] = useState({
+    photo: null
+  });
 
-const Upload = ({navigation}) => {
-    return (
-        <View style={styles.container}>
-            <Text> Upload Screen </Text>
-            <TouchableOpacity
-                style = {styles.button}
-                onPress = {() => navigation.navigate("Home")}>
-                <Text style={styles.buttontext}>Upload Media</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+  const handleChoosePhoto = () => {
+    const options = {
+      noData: true
+    };
+    ImagePicker.launchImageLibrary(options, response => {
+      if (response.uri) {
+        setPhoto({ photo: response });
+      }
+    });
+  };
+
+  const createFormData = (photo, body) => {
+    const data = new FormData();
+
+    data.append("photo", {
+      name: photo.fileName,
+      type: photo.type,
+      uri:
+        Platform.OS === "android" ? photo.uri : photo.uri.replace("file://", "")
+    });
+
+    Object.keys(body).forEach(key => {
+      data.append(key, body[key]);
+    });
+
+    return data;
+  };
+
+  const handleUploadPhoto = () => {
+    fetch("http://localhost:3000/api/upload", {
+      method: "POST",
+      body: createFormData(this.state.photo, { userId: "123" })
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log("upload succes", response);
+        alert("Upload success!");
+        this.setState({ photo: null });
+      })
+      .catch(error => {
+        console.log("upload error", error);
+        alert("Upload failed!");
+      });
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {photo && (
+        <React.Fragment>
+          <Image
+            source={{ uri: photo.uri }}
+            style={{ width: 300, height: 300 }}
+          />
+          <Button title="Upload" onPress={handleUploadPhoto} />
+        </React.Fragment>
+      )}
+      <Button title="Choose Photo" onPress={handleChoosePhoto} />
+    </View>
+  );
+};
 export default Upload;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'orange',
-    },
-    button_container: {
-        width: '100%',
-        flexDirection: 'row',
-        paddingBottom: 30,
-        paddingTop: 30,
-        backgroundColor: '#31e89e',
-        justifyContent: 'center'
-    },
-    buttontext: {
-        alignSelf: "center",
-        fontSize: 18,
-        fontWeight: "bold",
-        color: "black",
-        textTransform: "uppercase",
-    },
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "orange"
+  },
+  button_container: {
+    width: "100%",
+    flexDirection: "row",
+    paddingBottom: 30,
+    paddingTop: 30,
+    backgroundColor: "#31e89e",
+    justifyContent: "center"
+  },
+  buttontext: {
+    alignSelf: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "black",
+    textTransform: "uppercase"
+  }
+});
